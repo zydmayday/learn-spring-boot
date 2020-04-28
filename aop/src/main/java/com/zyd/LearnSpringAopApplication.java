e26424c99bbe1121cc8600dc37b119e5f2f8b2cc
autho <123@qq.com>
2020-04-28 22:24:40 +0900

    create a new project working with spring aop

---
+++ aop/src/main/java/com/zyd/LearnSpringAopApplication.java
@@ -0,0 +1,13 @@
+package com.zyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringAopApplication.class, args);
+	}
+
+}
