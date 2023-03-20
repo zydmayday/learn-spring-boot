e0e215ab951f54d39c2b5c881ebeec5fc7977f47
zyd <zydmayday@gmail.com>
2023-03-20 22:04:07 +0900

    onetoone example

--- graphql/src/main/java/com/zydmayday/graphql/GraphqlApplication.java
+++ graphql/src/main/java/com/zydmayday/graphql/GraphqlApplication.java
@@ -1,5 +1,6 @@
 package com.zydmayday.graphql;
 
+import jakarta.persistence.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.CrudRepository;
@@ -11,29 +12,72 @@ import org.springframework.stereotype.Repository;
 @SpringBootApplication
 public class GraphqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GraphqlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlApplication.class, args);
+    }
 
 }
 
-record Organization (String orgCode, String orgName) {}
+@Entity
+@Table(name = "organization")
+class Organization {
+    @Id
+    String orgCode;
+
+    String orgName;
+}
+
+@Entity
+@Table(name = "employee")
+class Employee {
+    @Id
+    String empNo;
+
+    @OneToOne
+    @JoinColumn(name = "level5_org_code")
+    Organization level5Org;
+
+    @OneToOne
+    @JoinColumn(name = "level6_org_code")
+    Organization level6Org;
+
+    @OneToOne
+    @JoinColumn(name = "level7_org_code")
+    Organization level7Org;
+}
 
 @Repository
 interface OrganizationRepository extends CrudRepository<Organization, String> {
-	Organization findByOrgCode(String orgCode);
+    Organization findByOrgCode(String orgCode);
+}
+
+@Repository
+interface EmployeeRepository extends CrudRepository<Employee, String> {
+    Employee findByEmpNo(String empNo);
 }
 
 @Controller
 class OrganizationGraphqlController {
 
-	public OrganizationGraphqlController(OrganizationRepository organizationRepository) {
-		this.organizationRepository = organizationRepository;
-	}
+    public OrganizationGraphqlController(OrganizationRepository organizationRepository,
+                                         EmployeeRepository employeeRepository) {
+        this.organizationRepository = organizationRepository;
+        this.employeeRepository = employeeRepository;
+    }
 
-	private OrganizationRepository organizationRepository;
-	@QueryMapping
-	public Organization organizationByOrgCode(@Argument String orgCode) {
-		return organizationRepository.findByOrgCode(orgCode);
-	}
+    private final OrganizationRepository organizationRepository;
+    private final EmployeeRepository employeeRepository;
+
+    @QueryMapping
+    public Organization organizationByOrgCode(@Argument String orgCode) {
+        System.out.println(organizationRepository.findAll());
+        Organization org = organizationRepository.findByOrgCode(orgCode);
+        System.out.println(org);
+        return org;
+    }
+
+    @QueryMapping
+    public Employee employeeByEmpNo(@Argument String empNo) {
+        return employeeRepository.findByEmpNo(empNo);
+    }
 }
--- graphql/src/main/resources/data.sql
+++ graphql/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into Organization (org_code, org_name) values ('org1', 'org1'),('org2', 'org2'),('org3', 'org3')
+insert into ORGANIZATION  (org_code, org_name) values ('orgCode1', 'orgName1'),('orgCode2', 'orgName2'),('orgCode3', 'orgName3');
+
+insert into EMPLOYEE (emp_no, level5_org_code, level6_org_code, level7_org_code) values ('emp1', 'orgCode1', 'orgCode2', 'orgCode3');
